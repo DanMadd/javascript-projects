2efa5d67a3585b8d5651d506a9a317fb249574ff
DanMadd <danmaddison00@gmail.com>
2025-08-31 23:16:33 +0100

    Added JS logic for adding book and div dynamically

--- scripts/library.js
+++ scripts/library.js
@@ -1,14 +1,61 @@
-document.getElementById("add-book-button").addEventListener("click",addNewBook)
+const libraryArray = [];
+const addOpenDialogButton = document.getElementById("open-dialog-add-book-button");
+const cancelButton = document.getElementById("cancel-book-button");
+const addBookDialog = document.getElementById("add-book-dialog");
+const addBookButton = document.getElementById("add-book-button");
 
-const myLibrary = [];
+addOpenDialogButton.addEventListener("click", () => {
+    addBookDialog.showModal();
+});
+
+cancelButton.addEventListener("click", () => {
+    tidyUpFields();
+    addBookDialog.close();
+})
+
+addBookButton.addEventListener("click", addNewBook)
 
 function Book(name, author, pages) {
+    this.id = crypto.randomUUID();
     this.name = name;
     this.author = author;
     this.pages = pages;
 }
 
 function addNewBook(){
-    console.log("button heard")
+    let newBookTitle = document.getElementById("Book").value;
+    let newBookAuthor = document.getElementById("Author").value;
+    let newBookPages = Number(document.getElementById("Pages").value);
+
+    if(newBookTitle.trim() !== "" && newBookAuthor.trim() !== "" && newBookPages > 0) {
+        const newBook = new Book(newBookTitle, newBookAuthor, newBookPages)
+        libraryArray.push(newBook);
+        
+        const newCard = document.createElement("div");
+        newCard.className = "content-card";
+        newCard.innerHTML = `
+            <strong>${newBook.name}</strong><br>
+            Author: ${newBook.author}<br>
+            Pages: ${newBook.pages}
+        `;
+
+        const oldCard = document.getElementById("open-dialog-add-book-button").parentElement;
+        console.log(oldCard);
+        oldCard.parentElement.insertBefore(newCard, oldCard);
+
+
+        tidyUpFields();
+        addBookDialog.close();
+        
+        console.log(newBook);
+        console.log(libraryArray);
+    } else {
+        window.alert("Not all values entered");
+    }
 }
 
+function tidyUpFields() {
+    document.getElementById("Book").value = "";
+    document.getElementById("Author").value = "";
+    document.getElementById("Pages").value = 0;
+}
